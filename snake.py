d169062fc868b28331046d9f88621971fe91f85c
Tsveti <tsvetipopova97@gmail.com>
2017-07-10 00:27:20 +0100

    Added fill and display update in the loop

--- snake.py
+++ snake.py
@@ -1,20 +1,22 @@
 import pygame
 
 pygame.init()
+black = (0,0,0)
+white = (255,255,255)
 
 # Create Surface
 gaveDisplay = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption("The Snake") 
 
-pygame.display.update()  #Update surface
-
 gameExit = False
-while not gameExit:   
+while not gameExit:     # Main loop
     for event in pygame.event.get():
         # Quit game
         if event.type == pygame.QUIT:
             gameExit = True
+    gameDisplay.fill(black)
+    pygame.display.update()
 
 pygame.quit()  # Uninitialize pygame
 quit() # Uninitialize python
